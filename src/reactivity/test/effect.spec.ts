4ee71873357d4defc0cb5708787d4b1f7a64d9b1
ccq <2902587206@qq.com>
2023-02-15 16:04:49 +0800

    edit

--- src/reactivity/test/effect.spec.ts
+++ src/reactivity/test/effect.spec.ts
@@ -33,7 +33,7 @@ describe('effect', () => {
     expect(nextAge).toBe(12)
   })
 
-  it('should return runner when call effect', () => {
+  it('should return runner when call effect ', () => {
     // 执行effect传递一个fn，effect会返回一个方法 runner，执行runner再次执行fn 并且返回fn的返回值
     let foo = 10
 
